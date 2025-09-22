f9ecad2c4f55b33deb182f72057d8b7572c5f0dc
Kassett1 <killian.touchard1@gmail.com>
2025-09-22 12:24:42 +0200

    theme toggle  button

---
+++ frontend/src/components/ThemeToggle.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const changeTheme = () => {
+  document.documentElement.classList.toggle("dark");
+}
+</script>
+
+<template>
+  <button @click="changeTheme"><img alt="Vue logo" src="@/assets/logo.svg" class="w-10 h-auto" /></button>
+</template>
